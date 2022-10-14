cd8a3923fd15b9918380af7c760bd0c605e40803
Thiago <fthiagorodrigues10@gmail.com>
2022-10-14 15:42:43 -0300

    entrega

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,57 +1,96 @@
 function getUser() {
-	const getUserForm = document.getElementById("getUserName");
+	const form = document.getElementById("getUserName");
 	const formInput = document.getElementById("user");
 	const searchButton = document.getElementById("search");
 	const errorText = document.querySelector(".error-text");
 
-	let localStorageProfiles = [];
+	if (formInput && searchButton) {
+		formInput.addEventListener("input", validateInput);
+		form.addEventListener("submit", getURL);
 
-	formInput.addEventListener("input", validateInput);
-	getUserForm.addEventListener("submit", getURL);
+		async function getURL(e) {
+			e.preventDefault();
 
-	function getURL(e) {
-		e.preventDefault();
+			try {
+				searchButton.classList.add("loading");
+				const response = await fetch(
+					`https://api.github.com/users/${formInput.value}`
+				);
 
-		const userURL = `https://api.github.com/users/${formInput.value}`;
+				if (response.ok) {
+					const githubUser = await response.json();
 
-		searchButton.classList.add("loading");
+					if (githubUser) {
+						setLocalStorage(githubUser);
+						localStorage.setItem("searched-value", JSON.stringify(githubUser));
+						window.location.href = "../profile/index.html";
+					}
+				} else {
+					errorText.style.display = "block";
+				}
+			} finally {
+				searchButton.classList.remove("loading");
+			}
+		}
 
-		const response = fetch(userURL)
-			.then((r) => {
-				return r.json();
-			})
-			.catch((err) => {
-				console.log(err);
-				errorText.style.display = "block";
-				localStorageProfiles.splice(1);
-				localStorage.setItem("users", JSON.stringify(localStorageProfiles));
-			});
-
-		localStorageProfiles.push(response);
-		localStorage.setItem("users", JSON.stringify(localStorageProfiles));
-	}
-
-	function validateInput() {
-		if (formInput.value.length > 0) {
-			formInput.classList.remove("error");
-			searchButton.removeAttribute("disabled");
-			errorText.style.display = "none";
-		} else {
-			searchButton.setAttribute("disabled", "");
-			formInput.classList.add("error");
+		function validateInput() {
+			if (formInput.value.length > 0) {
+				formInput.classList.remove("error");
+				searchButton.removeAttribute("disabled");
+				errorText.style.display = "none";
+			} else {
+				searchButton.setAttribute("disabled", "");
+				formInput.classList.add("error");
+			}
 		}
 	}
 }
 
 getUser();
 
+function setLocalStorage(user) {
+	const usersArray = JSON.parse(localStorage.getItem("users")) || [];
+
+	if (!usersArray.find((item) => item.login === user.login)) {
+		usersArray.push(user);
+		localStorage.setItem("users", JSON.stringify(usersArray));
+	}
+}
+
 function recentlySearched() {
 	const profilesList = document.getElementById("profilesList");
+	const profiles = JSON.parse(localStorage.getItem("users")).splice(-3);
 
-	const profiles = JSON.parse(localStorage.getItem("users"));
+	profiles.forEach((item) => {
+		profilesList.insertAdjacentHTML(
+			"beforeend",
+			`
+		<li class="list-item">
+      <img src="${item.avatar_url}" alt="">
+      <a href="../profile/index.html" id="${item.login}">Acessar este perfil</a>
+    </li>`
+		);
+	});
 
-	if (profiles) {
-		console.log(profiles);
-	}
+	const profilesLink = profilesList.querySelectorAll(".list-item img");
+
+	profilesLink.forEach((link) => {
+		link.addEventListener("mouseover", (e) => {
+			const user = profiles.find(
+				(item) => item.login === e.target.nextElementSibling.getAttribute("id")
+			);
+
+			localStorage.setItem("searched-value", JSON.stringify(user));
+		});
+
+		link.addEventListener("mouseover", handleHover);
+
+		function handleHover(e) {
+			profilesLink.forEach((link) => {
+				link.parentElement.classList.remove("hover");
+			});
+			e.target.parentElement.classList.add("hover");
+		}
+	});
 }
 recentlySearched();
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,53 @@
-/* Desenvolva sua lógica aqui...*/
+function profile() {
+	const user = JSON.parse(localStorage.getItem("searched-value"));
+	
+	function header() {
+		const header = document.querySelector("header");
+
+		header.innerHTML = `<div class="user-profile">
+      <div class="user-image">
+        <img src="${user.avatar_url}" alt="">
+     </div>
+     <div class="user-info">
+       <h1>${user.name}</h1>
+        <span>${user.bio ?? "Github User"}</span>
+     </div>
+    </div>
+
+    <ul class="links">
+      <li><a class="btn btn-primary" href="${
+				user.email ?? user.html_url
+			}">Email</a></li>
+      <li><a class="btn btn-grey" href="../home/index.html">Trocar de usuário</a></li>
+    </ul>`;
+	}
+	header();
+
+	async function repository() {
+		const grid = document.getElementById("repos");
+
+		const response = await fetch(user.repos_url);
+		const repos = await response.json();
+
+		repos.forEach((rep) => {
+			grid.insertAdjacentHTML(
+				"beforeend",
+				`
+      <li class="card">
+      <h2>${rep.name}</h2>
+      <p>${rep.description ?? "Repositório"}</p>
+      <div class="card-links">
+        <a class="btn btn-outline" href="${
+					rep.html_url
+				}" target="_blank">Repositório</a>
+        <a class="btn btn-outline" href="${
+					rep.homepage
+				}" target="_blank">Demo</a>
+      </div>
+    </li>`
+			);
+		});
+	}
+	repository();
+}
+profile();
